875bfe0392dd55be9491e5b04265ddacb585862c
Derek Myers <cashmerelc@gmail.com>
2024-04-29 19:31:20 +0200

    Add event listeners to next and prev buttons and fetch the characters on the corresponding page. Also disable prev button on app load since we are already on the first page and there is no previous page. When we reach the last page, the next button is disabled

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { createCharacterCard } from "./components/card/card.js";
 
+console.clear();
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -11,19 +12,23 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
+let url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+prevButton.setAttribute("disabled", "");
 
 async function fetchCharacters() {
-  let url = "https://rickandmortyapi.com/api/character/";
+  console.log(url);
   try {
     const response = await fetch(url);
     const data = await response.json();
+    maxPage = data.info.pages;
+    console.log(data);
+    console.log(maxPage);
     const characters = data.results;
     characters.forEach((character) => {
       createCharacterCard(cardContainer, character);
-      console.log(character);
     });
   } catch (err) {
     console.log(err);
@@ -31,3 +36,33 @@ async function fetchCharacters() {
 }
 
 fetchCharacters();
+
+nextButton.addEventListener("click", () => {
+  // Increment the page counter
+  page++;
+  // Check to see if the new page count is the max page number. If so, disable the next button
+  if (page === maxPage) {
+    nextButton.setAttribute("disabled", "");
+  }
+  // The prevButton starts out disabled, so once we move to the next page we need to enable the prevButton
+  if (page === 2) {
+    prevButton.disabled = false;
+  }
+  url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+  fetchCharacters();
+});
+
+prevButton.addEventListener("click", () => {
+  // Decrement the page counter
+  page--;
+  // Disable the prevButton when we reach the first page (after we have already left the first page)
+  if (page === 1) {
+    prevButton.setAttribute("disabled", "");
+  }
+  // When we reach the maxpage, we disable the next button. If we go to the previous page, we need to enable it again
+  if (page === maxPage - 1) {
+    nextButton.disabled = false;
+  }
+  url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+  fetchCharacters();
+});
